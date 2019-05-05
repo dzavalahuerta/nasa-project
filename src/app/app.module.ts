4124723b27b41fd4a98cf4478603cb55bb5fd63c
DanielZavalaHuerta <danielz123789@gmail.com>
2019-05-04 23:54:32 -0700

    added bootstrap in angular.json file, created apod and navbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { APODComponent } from './apod/apod.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    APODComponent
   ],
   imports: [
     BrowserModule
