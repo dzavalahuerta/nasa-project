3ef20ee042669c8175bb7ae392f1eb1d31e36c55
DanielZavalaHuerta <danielz123789@gmail.com>
2019-05-24 14:20:18 -0700

    each sol can now be queried and all of its photos will show

--- src/app/rover-components/curiosity/curiosity.component.ts
+++ src/app/rover-components/curiosity/curiosity.component.ts
@@ -1,28 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ServerService } from 'src/app/services/server.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-curiosity',
   templateUrl: './curiosity.component.html',
   styleUrls: ['./curiosity.component.css']
 })
-export class CuriosityComponent implements OnInit {
+export class CuriosityComponent implements OnInit, OnDestroy {
+  solSelectorForm: FormGroup;
   infiniteScrollToggle = false;
   missionManifest;
   currentSol: number;
   currentPageOfSol: number;
   totalPhotosInCurrentSol: number;
-
   photosOfCurrentSol = [];
+  solDoesNotExistCase = [{"img_src": "https://i.stack.imgur.com/m6Gv4.png"}];
 
   constructor(private serverService: ServerService) { }
 
-  ngOnInit() {
-    // i might not keep this
-    // document.body.style.backgroundColor = 'rgba(234, 224, 200, 0.938)';
+  checkPageYOffset(){
+    if(window.pageYOffset > 1000){
+      return true;
+    }
+    else if(window.pageYOffset <= 1000){
+      return false;
+    }
+  }
 
-    this.currentSol = 2;
+  ngOnInit() {
+    document.body.style.backgroundColor = 'rgba(234, 224, 200, 0.938)';
+
+    window.addEventListener('scroll', this.checkPageYOffset, true);
+
+    this.solSelectorForm = new FormGroup({
+      'selectedSol': new FormControl(null, Validators.required, this.invalidSol.bind(this))
+    });
+
+    this.currentSol = 1;
     this.currentPageOfSol = 1;
+    
+    this.serverService.getPageOfPhotosOfSol('curiosity', this.currentSol, this.currentPageOfSol)
+      .subscribe(
+        (photos: [])=>{
+          this.photosOfCurrentSol = photos;
+        }
+      );
 
     this.serverService.getMissionManifest('curiosity')
       .subscribe(
@@ -49,36 +73,109 @@ export class CuriosityComponent implements OnInit {
           }   
         }
       );
-
-    this.serverService.getPageOfPhotosOfSol('curiosity', this.currentSol, this.currentPageOfSol)
-      .subscribe(
-        (photos: [])=>{
-          this.photosOfCurrentSol = photos;
-        }
-      );
   }
 
   getNextPageOfPhotos(){
     this.infiniteScrollToggle = true;
+    console.log(`start`);
 
     let maximumPagesOfPhotosForCurrentSol = Math.ceil(this.totalPhotosInCurrentSol/25);
     
-    if(this.currentPageOfSol === maximumPagesOfPhotosForCurrentSol){
+    if(this.currentPageOfSol === maximumPagesOfPhotosForCurrentSol || maximumPagesOfPhotosForCurrentSol === 0){
+      console.log('1 runs');
+      this.infiniteScrollToggle = false;
       return;
     }
     else{
+      console.log('2 runs');
       this.currentPageOfSol += 1;
       this.serverService.getPageOfPhotosOfSol('curiosity', this.currentSol, this.currentPageOfSol)
       .subscribe(
         (nextPageOfPhotos: [])=>{
+          console.log(`the sub runs`);
           nextPageOfPhotos.forEach((photo, index) => {
             this.photosOfCurrentSol.push(photo);
             if(index === nextPageOfPhotos.length-1){
               this.infiniteScrollToggle = false;
             }
           });
+        },
+        ()=>{
+          console.log(`the error runs`);
+          this.currentPageOfSol -= 1;
+          this.infiniteScrollToggle = false;
         }
       );
     }
+  }
+
+  getSpecificSol(){
+    this.currentSol = this.solSelectorForm.value.selectedSol;
+    this.currentPageOfSol = 1;
+
+    let allPhotosFromRover = this.missionManifest.photos;
+      if(this.currentSol > allPhotosFromRover.length/2){
+        for (let i = allPhotosFromRover.length-1; i >= 0; i--) {
+          let element = allPhotosFromRover[i];
+          if(element.sol === this.currentSol){
+            this.totalPhotosInCurrentSol = element.total_photos;
+            break;
+          }
+          else{
+            this.totalPhotosInCurrentSol = 0;
+          }
+        }
+      }
+      else{
+        for (let i = 0; i < allPhotosFromRover.length; i++) {
+          let element = allPhotosFromRover[i];
+          if(element.sol === this.currentSol){
+            this.totalPhotosInCurrentSol = element.total_photos;
+            break;
+          }
+          else{
+            this.totalPhotosInCurrentSol = 0;
+          }
+        }
+      }
+
+    this.serverService.getPageOfPhotosOfSol('curiosity', this.currentSol, this.currentPageOfSol)
+      .subscribe(
+        (photos: [])=>{
+          if(photos.length === 0){
+            this.photosOfCurrentSol = this.solDoesNotExistCase;
+          }
+          else{
+            this.photosOfCurrentSol = photos;
+          }
+        }
+      );    
+  }
+
+  scrollUp(){
+    window.scroll({
+      top: 700,
+      behavior: 'smooth'
+    });
+  }
+
+  invalidSol(control: FormControl): Promise<any> | Observable<any>{
+    let maxSol = this.missionManifest.max_sol;
+    const promise = new Promise<any>((resolve, reject)=>{
+      setTimeout(() => {        
+        if(control.value < 1 || control.value > maxSol){
+          resolve({'invalid sol': true});
+        }
+        else{
+          resolve(null);
+        }
+      }, 50);
+    });
+    return promise;
+  }
+
+  ngOnDestroy(){
+    window.scrollTo(0,0);
+    window.removeEventListener('scroll', this.checkPageYOffset, true);
   }
 }
