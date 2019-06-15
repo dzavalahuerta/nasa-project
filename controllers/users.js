c132484d99fc656307194b6d9d66ab5e011e7195
DanielZavalaHuerta <danielz123789@gmail.com>
2019-06-15 15:06:12 -0700

    added jsonwebtokens and a function to sign the token so that the user now has authentication

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,15 @@
+const JWT = require('jsonwebtoken');
 const User = require('../models/user');
+const { JWT_SECRET } = require('../configuration');
+
+signToken = user =>{
+    return JWT.sign({
+        iss: 'NASA-project',
+        sub: user._id,
+        iat: new Date().getTime(),
+        exp: new Date().setDate(new Date().getDate()+1)
+    }, JWT_SECRET);
+};
 
 module.exports = {
     signUp: async(req,res,next)=>{
@@ -11,11 +22,13 @@ module.exports = {
         const newUser = new User({ email, password });
         await newUser.save();
 
-        res.json({user: "created"});
+        const token = signToken(newUser);
+
+        res.status(200).json({token});
     },
     
     signIn: async(req,res,next)=>{
-        console.log(`UsersController.signIn() worked`);
+
     },
 
     secret: async(req,res,next)=>{
